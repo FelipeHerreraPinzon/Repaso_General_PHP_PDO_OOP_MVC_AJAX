3b8ca01135a59d5444a8de52ad785ee87c8404d2
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2023-12-05 16:13:50 -0500

    clases y metodos abstractos

---
+++ clases_abstractas/ClassCliente.php
@@ -0,0 +1,48 @@
+<?php
+    require_once("ClassPersona.php");
+
+    class Cliente extends Persona{
+
+        protected $fltCredito; 
+
+        function __construct(int $dpi, string $nombre, int $edad)
+        {
+             parent::__construct($dpi, $nombre, $edad);
+        }
+
+        public function setCredito(string $credito){
+            $this->fltCredito = $credito;
+        }
+
+        public function getCredito():float
+        {
+           return $this->fltCredito;
+        }
+
+        public function getDatosPersonales()  /// redefiniendo metodo abstracto
+        {
+            $datos = "
+                     <h2>DATOS PERSONALES</h2>
+                     DPI: {$this->intDPI} <br>
+                     Nombre: {$this->strNombre} <br>
+                     Edad: {$this->intEdad} <br>
+            
+            ";
+            return $datos;  
+        }
+
+        public function setMensaje(string $mensaje)   // parametro string $mensaje
+        {
+            $this->mensaje = $mensaje;
+        }
+        public function getMensaje():string  // devuelve un string $mensaje
+        {
+            return $this->mensaje.''.$this->strNombre;
+        } 
+
+
+    }
+
+
+
+?>
---
+++ clases_abstractas/ClassPersona.php
@@ -0,0 +1,36 @@
+<?php
+
+
+   abstract  class Persona  /// una clase abstracta NO permite instanciar objetos
+    {
+       public $intDPI;
+       public $strNombre;
+       public $intEdad;
+       public $mensaje;
+    
+       function __construct(int $dpi, string $nombre, int $edad)
+       {
+            $this->intDPI = $dpi;
+            $this->strNombre = $nombre;
+            $this->intEdad = $edad;
+       }
+
+
+     abstract  public function getDatosPersonales();  /// los metodos abstractos no pueden tener cuerpo
+    /*
+       {   $datos = "
+                    <h2>DATOS PERSONALES</h2>
+                    DPI: {$this->intDPI} <br>
+                    Nombre: {$this->strNombre} <br>
+                    Edad: {$this->intEdad} <br>
+           ";
+           return $datos;  
+       }
+    */
+     abstract  public function setMensaje(string $mensaje);   // parametro string $mensaje
+     abstract  public function getMensaje():string;   // devuelve un string $mensaje
+ 
+    
+    }
+
+?>    
