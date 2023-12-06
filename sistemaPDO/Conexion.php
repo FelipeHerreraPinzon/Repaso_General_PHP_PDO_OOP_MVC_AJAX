21039161dee693e1f757121f6aad6e73dc6e01b6
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2023-12-06 02:36:04 -0500

    PDO c

---
+++ sistemaPDO/Conexion.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Conexion
+    {
+        private $host = "localhost";
+        private $user = "root";
+        private $password = "";
+        private $db = "db_sistema";
+        private $conect;
+
+        public function __construct()
+        {
+             $connectionString = "mysql:host".$this->host.";dbname=".$this->db.";charset=utf8";
+       
+        try
+        { 
+           $this->conect = new PDO($connectionString, $this->user, $this->password);
+           $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           // echo "conectado !!!";
+        } catch (Exception $e){
+            $this->conect = 'Error de conexion';
+            echo "ERROR: ". $e->getMessage();
+        }
+
+      }
+    }
+
+  $conect = new Conexion();
+?>
